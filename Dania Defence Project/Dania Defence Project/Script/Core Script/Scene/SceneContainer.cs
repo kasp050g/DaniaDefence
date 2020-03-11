91762b2c1daa8308a825392242c3555865af8ef2
kasp050g <57252832+kasp050g@users.noreply.github.com>
2020-03-11 12:34:51 +0100

    Now fix

--- Dania Defence Project/Dania Defence Project/Script/Core Script/Scene/SceneContainer.cs
+++ Dania Defence Project/Dania Defence Project/Script/Core Script/Scene/SceneContainer.cs
@@ -20,7 +20,41 @@ namespace Dania_Defence_Project
 
 		public void MakeScenes()
 		{
+			PickScene pickScene = new PickScene
+			{
+				Name = "Pick Scene"
+			};
+			Scenes.Add(pickScene);
 
+			MainScene mainScene = new MainScene
+			{
+				Name = "Main Scene"
+			};
+			Scenes.Add(mainScene);
+
+			Kasper_Scene kasper_Scene = new Kasper_Scene
+			{
+				Name = "Kasper Scene"
+			};
+			Scenes.Add(kasper_Scene);
+
+			Casper_Scene casper_Scene = new Casper_Scene
+			{
+				Name = "Casper Scene"
+			};
+			Scenes.Add(casper_Scene);
+
+			Nicolai_Scene nicolai_Scene = new Nicolai_Scene
+			{
+				Name = "Nicolai Scene"
+			};
+			Scenes.Add(nicolai_Scene);
+
+			Marius_Scene marius_Scene = new Marius_Scene
+			{
+				Name = "Marius Scene"
+			};
+			Scenes.Add(marius_Scene);
 		}
 	}
 }
