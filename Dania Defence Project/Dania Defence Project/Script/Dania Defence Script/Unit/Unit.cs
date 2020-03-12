88362c78ce9a0049781e0594df56973c77a79d2e
Nicolai <nicolai240492@hotmail.com>
2020-03-12 11:52:57 +0100

    unit class

---
+++ Dania Defence Project/Dania Defence Project/Script/Dania Defence Script/Unit/Unit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dania_Defence_Project.Script.Dania_Defence_Script.Unit
+{
+    class Unit
+    {
+        private knowledge = new knowledge;
+    }
+}
