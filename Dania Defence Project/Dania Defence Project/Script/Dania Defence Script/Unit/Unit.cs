ba9f3250dd41a5d56034304f264565e3b78b3ec6
Casper <Seidelin2@hotmail.com>
2020-03-16 13:17:56 +0100

    WaveController, Unit

--- Dania Defence Project/Dania Defence Project/Script/Dania Defence Script/Unit/Unit.cs
+++ Dania Defence Project/Dania Defence Project/Script/Dania Defence Script/Unit/Unit.cs
@@ -6,8 +6,23 @@ using System.Threading.Tasks;
 
 namespace Dania_Defence_Project
 {
-    public class Unit
+    public class Unit : GameObject
     {
+        public event Action onUnitDeath;
+        
+        public void UnitDeath()
+        {
+            if (onUnitDeath != null)
+            {
+                onUnitDeath();
+            }
+        }
 
+        public override void Start()
+        {
+            base.Start();
+        }
+
+        
     }
 }
--- Dania Defence Project/Dania Defence Project/Script/Dania Defence Script/Wave Controller/WaveController.cs
+++ Dania Defence Project/Dania Defence Project/Script/Dania Defence Script/Wave Controller/WaveController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,21 +10,39 @@ namespace Dania_Defence_Project
 {
     public class WaveController : Component
     {
-        WaveContainer waveContainer = new WaveContainer();
 
+
+
+        WaveContainer waveContainer = new WaveContainer();
+        Wave nextWave;
         int currentWave = 0;
 
-        
+        bool jamrufuf = false;
+        float currentCoundownTime;
+        string countdownText = "COUNTDOWN";
+        GuiText countdownUIText;
 
         public override void Awake()
         {
             base.Awake();
+            countdownUIText = new GuiText(SpriteContainer.normalFont,
+                countdownText,
+                new Vector2(GraphicsSetting.ScreenSize.X / 2, GraphicsSetting.ScreenSize.Y / 2 - 200),
+                new Vector2(1,1), 
+                1,
+                OriginPositionEnum.Mid );
+
+            countdownUIText.IsActive = false;
+            Instantiate(countdownUIText);
 
         }
 
         public override void Start()
         {
             base.Start();
+            Unit unit = new Unit();
+
+            
         }
 
         public override void Update()
@@ -32,6 +51,50 @@ namespace Dania_Defence_Project
             base.Update();
 
         }
+
+        public void CountdownUntilWave()
+        {
+            if (currentCoundownTime > 0)
+            {
+                currentCoundownTime -= Time.deltaTime;
+
+                if (currentCoundownTime <= 0)
+                {
+                    SpawnUnit();
+                }
+            }
+
+
+        }
+        public void SpawnUnit()
+        {
+            // Instantiate Unit
+            nextWave = waveContainer.Waves[currentWave];
+
+            foreach (Unit item in nextWave.Units)
+            {
+                item.Transform.Position = nextWave.Positions[0];
+                Instantiate(item);
+            }
+        }
+
+        public void StartNewWave()
+        {
+            currentCoundownTime = 10;
+
+
+        }
+
+        public void OnUnitDeath()
+        {
+            
+        }
+
+        
+
+        
+
+
 
 
     }
