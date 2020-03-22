f9ee13b3b8860ce4173bb8f32e42a4cc0609de49
Kasper Home <Kasper_fs@hotmail.com>
2020-03-22 10:46:52 +0100

    Towers Script add

---
+++ Dania Defence Project/Dania Defence Project/Script/Dania Defence Script/TowerFolder/TowerProjectile.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dania_Defence_Project
+{
+	public class TowerProjectile : GameObject
+	{
+		public float rotationVelocity = 3f;
+		public float LinearVelocity = 4f;
+
+		public Vector2 myTarget;
+
+        public float speed = 100;
+        public Vector2 velocity;
+
+        #region Constructor
+        public TowerProjectile(Texture2D _sprite, Vector2 _position, Vector2 _scale, float _layerDepth, 
+			OriginPositionEnum _originPositionEnum, float _speed)
+		{
+			this.sprite = _sprite;
+			this.Transform.Position = _position;
+			this.Transform.Scale = _scale;
+			this.Transform.Scale = _scale;
+			this.layerDepth = _layerDepth;
+			this.OriginPositionEnum = _originPositionEnum;
+			this.speed = _speed;
+		}
+		#endregion
+
+		#region Method
+		public override void Awake()
+		{
+			base.Awake();
+		}
+
+		public override void Start()
+		{
+			base.Start();
+		}
+
+		public override void Update()
+		{
+			UpdateProjectile();
+			ProjectileMovement();
+
+			float angle1 = Helper.CalculateAngleBetweenPositions(transform.Position, myTarget);
+
+			transform.Rotation = angle1 - 90;
+
+			base.Update();
+		}
+
+		public override void Draw(SpriteBatch spriteBatch)
+		{
+			base.Draw(spriteBatch);
+		}
+
+
+        public void UpdateProjectile()
+        {
+            float deltaTime = Time.deltaTime;
+
+            transform.Position += ((velocity * speed) * deltaTime);
+        }
+
+        public void ProjectileMovement()
+        {
+            Vector2 tmpDirection = new Vector2(0, 0);
+
+            MouseState state = Mouse.GetState();
+
+            var mouseX = state.Position.X;
+            var mouseY = state.Position.Y;
+
+            Vector2 newPosition = new Vector2(mouseX, mouseY);
+
+            Vector2 worldPosition = Vector2.Transform(newPosition, Matrix.Invert(SceneController.Camera.Transform));
+
+            myTarget = worldPosition;
+
+            if (transform.Position.X < worldPosition.X)
+            {
+                tmpDirection += new Vector2(1, 0);
+            }
+
+            if (transform.Position.X > worldPosition.X)
+            {
+                tmpDirection += new Vector2(-1, 0);
+            }
+
+            if (transform.Position.Y < worldPosition.Y)
+            {
+                tmpDirection += new Vector2(0, 1);
+            }
+
+            if (transform.Position.Y > worldPosition.Y)
+            {
+                tmpDirection += new Vector2(0, -1);
+            }
+
+            tmpDirection.Normalize();
+
+            velocity = tmpDirection;
+        }
+        #endregion
+    }
+}
---
+++ Dania Defence Project/Dania Defence Project/Script/Dania Defence Script/TowerFolder/Towers/Dennis_Tower.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Dania_Defence_Project
+{
+	public class Dennis_Tower : Towers
+	{
+		#region Constructors
+		public Dennis_Tower(Texture2D _sprite, Vector2 _position, Vector2 _scale, float _layerDepth, 
+			OriginPositionEnum _originPositionEnum, float _speed, float _fireRate) : base(_sprite, _position, _scale, _layerDepth, _originPositionEnum, _speed, _fireRate)
+		{
+		}
+
+		public Dennis_Tower()
+		{
+
+		}
+
+		public void MakeTower()
+		{
+			sprite = SpriteContainer.sprite["Dennis"];
+			transform.Position = new Vector2(-360, 330);
+			Transform.Scale = new Vector2(0.25f, 0.25f);
+			layerDepth = 0.25f;
+			OriginPositionEnum = OriginPositionEnum.BottomMid;
+			this.speed = 60;
+			fireRate = 1f;
+		}
+
+		public override void Projectile()
+		{
+			myProjectile = new TowerProjectile(
+				//Texture
+				SpriteContainer.sprite["PDennis"],
+
+				//Position
+				new Vector2(transform.Position.X, transform.Position.Y - 75),
+
+				//Scale
+				new Vector2(0.5f, 0.5f),
+
+				//Layer Depth
+				0.2f,
+
+				//Origin
+				OriginPositionEnum.Mid,
+
+				//Speed
+				this.speed
+				);
+
+			Instantiate(myProjectile);
+
+			base.Projectile();
+		}
+		#endregion
+
+		#region Methods
+		public override void Awake()
+		{
+			MakeTower();
+			base.Awake();
+		}
+
+		public override void Start()
+		{
+			base.Start();
+		}
+
+		public override void Update()
+		{
+			base.Update();
+		}
+
+		public override void Draw(SpriteBatch spriteBatch)
+		{
+			base.Draw(spriteBatch);
+		}
+		#endregion
+	}
+}
